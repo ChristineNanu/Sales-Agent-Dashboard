df50f7edc58f92af7c1d564beef2690a462dc3b1
christinemundi <mundichristine@gmail.com>
2024-05-15 20:30:41 +0300

    dependancies installed

---
+++ sales-agent-dashboard/src/app/dashboard/signups/signups.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signups',
+  standalone: true,
+  imports: [],
+  templateUrl: './signups.component.html',
+  styleUrl: './signups.component.css'
+})
+export class SignupsComponent {
+
+}
